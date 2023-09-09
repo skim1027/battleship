6c8c4b50645c1180fd002702ef3412ff8d569de7
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-09-08 20:44:15 -0600

    feat: Add board class and spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,18 @@
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+RSpec.describe Board do 
+  describe '#initialize' do
+    it 'exists' do
+      board = Board.new
+      expect(board).to be_instance_of(Board)
+    end
+    it 'board has cells' do
+      board = Board.new
+      expect(board.cells).to be_instance_of(Hash)
+      expect(board.cells.keys.count).to eq(16)
+      expect(board.cells.values.first.class).to eq(Cell)
+    end
+  end
+end
