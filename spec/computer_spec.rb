3ca6e25e9715646524b6b39f395a5d99e1d29068
Sooyung Kim <kimx1279@gmail.com>
2023-09-11 16:56:57 -0600

    test: add framework for test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,2 +1,36 @@
 require './spec/spec_helper'
 
+RSpec.describe Computer do
+  before :each do
+    # @computer = Computer.new
+    # @board = Board.new
+  end
+  
+  describe '#initialize' do
+    it 'initialize' do
+     # expect(@computer).to be_instance_of(Computer)
+    end
+  end
+
+  describe '#create_coordinate' do
+    it 'create coordinate for cruiser' do
+
+    end
+
+    it 'create coordinates for submarine' do
+
+    end
+  end
+
+  describe '#place_ship' do
+    it 'places ship in random' do
+      
+    end
+  end
+
+  describe '#take_turn' do
+    it 'choose a coordinate to fire on' do
+
+    end
+  end
+end
